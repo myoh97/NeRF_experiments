467e49fe3deeafebde8179657d10c72bb68647da
myoh97 <mmyy2513@unist.ac.kr>
2023-02-03 06:21:30 +0000

    N_iter

--- run_nerf.py
+++ run_nerf.py
@@ -433,6 +433,7 @@ def config_parser():
                         help='input data directory')
 
     parser.add_argument('--rgb', type=str, help='rgb order', default='rgb')
+    parser.add_argument('--N_iter', type=int, help='number of total iterations', default=100000)
     
     # training options
     parser.add_argument("--netdepth", type=int, default=8, 
@@ -701,7 +702,7 @@ def train():
         rays_rgb = torch.Tensor(rays_rgb).to(device)
 
 
-    N_iters = 200000 + 1
+    N_iters = args.N_iter + 1
     print('Begin')
     print('TRAIN views are', i_train)
     print('TEST views are', i_test)
@@ -808,8 +809,8 @@ def train():
                 rgbs, disps = render_path(render_poses, hwf, K, args.chunk, render_kwargs_test)
             print('Done, saving', rgbs.shape, disps.shape)
             moviebase = os.path.join(basedir, expname, '{}_spiral_{:06d}_'.format(expname, i))
-            imageio.mimwrite(moviebase + 'rgb.mp4', to8b(rgbs), fps=30, quality=8)
-            imageio.mimwrite(moviebase + 'disp.mp4', to8b(disps / np.max(disps)), fps=30, quality=8)
+            imageio.mimwrite(moviebase + 'rgb.mp4', to8b(rgbs), fps=30, quality=10)
+            imageio.mimwrite(moviebase + 'disp.mp4', to8b(disps / np.max(disps)), fps=30, quality=10)
 
             # if args.use_viewdirs:
             #     render_kwargs_test['c2w_staticcam'] = render_poses[0][:3,:4]
@@ -826,51 +827,8 @@ def train():
                 render_path(torch.Tensor(poses[i_test]).to(device), hwf, K, args.chunk, render_kwargs_test, gt_imgs=images[i_test], savedir=testsavedir)
             print('Saved test set')
 
-
-    
         if i%args.i_print==0:
             tqdm.write(f"[TRAIN] Iter: {i} Loss: {loss.item()}  PSNR: {psnr.item()}")
-        """
-            print(expname, i, psnr.numpy(), loss.numpy(), global_step.numpy())
-            print('iter time {:.05f}'.format(dt))
-
-            with tf.contrib.summary.record_summaries_every_n_global_steps(args.i_print):
-                tf.contrib.summary.scalar('loss', loss)
-                tf.contrib.summary.scalar('psnr', psnr)
-                tf.contrib.summary.histogram('tran', trans)
-                if args.N_importance > 0:
-                    tf.contrib.summary.scalar('psnr0', psnr0)
-
-
-            if i%args.i_img==0:
-
-                # Log a rendered validation view to Tensorboard
-                img_i=np.random.choice(i_val)
-                target = images[img_i]
-                pose = poses[img_i, :3,:4]
-                with torch.no_grad():
-                    rgb, disp, acc, extras = render(H, W, focal, chunk=args.chunk, c2w=pose,
-                                                        **render_kwargs_test)
-
-                psnr = mse2psnr(img2mse(rgb, target))
-
-                with tf.contrib.summary.record_summaries_every_n_global_steps(args.i_img):
-
-                    tf.contrib.summary.image('rgb', to8b(rgb)[tf.newaxis])
-                    tf.contrib.summary.image('disp', disp[tf.newaxis,...,tf.newaxis])
-                    tf.contrib.summary.image('acc', acc[tf.newaxis,...,tf.newaxis])
-
-                    tf.contrib.summary.scalar('psnr_holdout', psnr)
-                    tf.contrib.summary.image('rgb_holdout', target[tf.newaxis])
-
-
-                if args.N_importance > 0:
-
-                    with tf.contrib.summary.record_summaries_every_n_global_steps(args.i_img):
-                        tf.contrib.summary.image('rgb0', to8b(extras['rgb0'])[tf.newaxis])
-                        tf.contrib.summary.image('disp0', extras['disp0'][tf.newaxis,...,tf.newaxis])
-                        tf.contrib.summary.image('z_std', extras['z_std'][tf.newaxis,...,tf.newaxis])
-        """
 
         global_step += 1
 
--- train.sh
+++ train.sh
@@ -1,5 +1,6 @@
-CUDA_VISIBLE_DEVICES=9 python run_nerf.py \
---config configs/lego.txt \
---N_rand 512 \
---rgb rgb \
---expname rgb_512
+CUDA_VISIBLE_DEVICES=9 python run_nerf.py --N_iter 100000 --config configs/lego.txt --N_rand 512 --rgb rgb --expname lego/rgb_512;
+CUDA_VISIBLE_DEVICES=9 python run_nerf.py --N_iter 100000 --config configs/lego.txt --N_rand 512 --rgb rbg --expname lego/rbg_512;
+CUDA_VISIBLE_DEVICES=9 python run_nerf.py --N_iter 100000 --config configs/lego.txt --N_rand 512 --rgb bgr --expname lego/bgr_512;
+CUDA_VISIBLE_DEVICES=9 python run_nerf.py --N_iter 100000 --config configs/lego.txt --N_rand 512 --rgb brg --expname lego/brg_512;
+CUDA_VISIBLE_DEVICES=9 python run_nerf.py --N_iter 100000 --config configs/lego.txt --N_rand 512 --rgb grb --expname lego/grb_512;
+CUDA_VISIBLE_DEVICES=9 python run_nerf.py --N_iter 100000 --config configs/lego.txt --N_rand 512 --rgb gbr --expname lego/gbr_512;
